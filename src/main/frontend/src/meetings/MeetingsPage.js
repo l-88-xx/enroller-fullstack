be9c6eb0d4a98ecd1919a15d20da6754ef7f9704
Lila <lila.88.xx@gmail.com>
2026-06-11 21:58:17 +0200

    animacja ładowania

--- src/main/frontend/src/meetings/MeetingsPage.js
+++ src/main/frontend/src/meetings/MeetingsPage.js
@@ -4,60 +4,90 @@ import NewMeetingForm from "./NewMeetingForm";
 import MeetingsList from "./MeetingsList";
 // obsługa błędów
 import { toast } from 'react-toastify';
+// ładowanie
+import Loader from '../components/Loader';
 
 export default function MeetingsPage({username}) {
     const [meetings, setMeetings] = useState([]);
+    const [loading, setLoading] = useState(false);
     const [addingNewMeeting, setAddingNewMeeting] = useState(false);
 
 // Pobieranie
 // useEffect wykonuje tylko przy pierwszym renderowaniu
 useEffect(() => {
-    const fetchMeetings = async () => {
-        const response = await fetch(`/api/meetings`);
-        if (response.ok) {
-            const meetings = await response.json();
-            setMeetings(meetings);
-        }else {
+ const fetchMeetings = async () => {
+     setLoading(true);
+     try {
+         const response = await fetch(`/api/meetings`);
+
+         if (response.ok) {
+             const meetings = await response.json();
+             setMeetings(meetings);
+         } else {
              toast.error('Błąd, nie pobrano spotkań');
          }
-    };
+     } finally {
+
+         setLoading(false);
+     }
+ };
     fetchMeetings();
 }, []);
 
 
 //Dodawanie spotkań
 async function handleNewMeeting(meeting) {
- const response = await fetch('/api/meetings', {
-     method: 'POST',
-     body: JSON.stringify(meeting),
-     headers: { 'Content-Type': 'application/json' }
- });
- if (response.ok) {
-     const newMeeting = await response.json();
-     const nextMeetings = [...meetings, newMeeting];
-     setMeetings(nextMeetings);
-     setAddingNewMeeting(false);
- }else {
-      const message = await response.text();
-      toast.error(message);
-  }
-}
-      // Usuwanie spotkań
-        async function handleDeleteMeeting(meeting) {
-            const response = await fetch(`/api/meetings/${meeting.id}`, {
-                method: 'DELETE',
-            });
-            if (response.ok) {
-                const nextMeetings = meetings.filter(m => m.id !== meeting.id);
-                setMeetings(nextMeetings);
-            }else {
-                 const message = await response.text();
-                 toast.error(message);
-             }
+    setLoading(true);
+    try {
+        const response = await fetch('/api/meetings', {
+            method: 'POST',
+            body: JSON.stringify(meeting),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if (response.ok) {
+            const newMeeting = await response.json();
+
+            const nextMeetings = [...meetings, newMeeting];
+
+            setMeetings(nextMeetings);
+            setAddingNewMeeting(false);
+        } else {
+            const message = await response.text();
+            toast.error(message);
         }
+    } finally {
+        setLoading(false);
+    }
+}
+
+      // Usuwanie spotkań
+       async function handleDeleteMeeting(meeting) {
+           setLoading(true);
+           try {
+               const response = await fetch(
+                   `/api/meetings/${meeting.id}`,
+                   {
+                       method: 'DELETE'
+                   }
+               );
+               if (response.ok) {
+                   const nextMeetings =
+                       meetings.filter(m => m.id !== meeting.id);
+                   setMeetings(nextMeetings);
+               } else {
+                   const message = await response.text();
+                   toast.error(message);
+               }
+           } finally {
+               setLoading(false);
+           }
+       }
 
         // zapisanie na spotkanie
         async function handleJoinMeeting(meeting) {
+        setLoading(true);
             try {
                 const response = await fetch(
                     `/api/meetings/${meeting.id}/participants`,
@@ -90,47 +120,79 @@ async function handleNewMeeting(meeting) {
                     toast.error(message);
                 }
             } catch (error) {
-                toast.error('Nie można połączyć się z serwerem');
-            }
+                  toast.error('Nie można połączyć się z serwerem');
+              }
+              finally {
+                  setLoading(false);
+              }
         }
 
         // wypisanie
-        async function handleLeaveMeeting(meeting) {
-            const response = await fetch(`/api/meetings/${meeting.id}/participants/${username}`,
-                {
-                    method: 'DELETE'
+async function handleLeaveMeeting(meeting) {
+
+    setLoading(true);
+
+    try {
+
+        const response = await fetch(
+            `/api/meetings/${meeting.id}/participants/${username}`,
+            {
+                method: 'DELETE'
+            }
+        );
+
+        if (response.ok) {
+
+            const nextMeetings = meetings.map(m => {
+                if (m.id === meeting.id) {
+                    return {
+                        ...m,
+                        participants: m.participants.filter(
+                            p => p.login !== username
+                        )
+                    };
                 }
-            );
-            if (response.ok) {
-                const nextMeetings = meetings.map(m => {
-                    if (m.id === meeting.id) {
-                        return {
-                            ...m, participants:m.participants.filter(p => p.login !== username)
-                        };
-                    }
-                    return m;
-                });
-                setMeetings(nextMeetings);
-            }else {
-                 const message = await response.text();
-                 toast.error(message);
-             }
+                return m;
+            });
+            setMeetings(nextMeetings);
+        } else {
+            const message = await response.text();
+            toast.error(message);
         }
-
-
+    } finally {
+        setLoading(false);
+    }
+}
     return (
         <div>
+
+            {loading && (
+                <div className="loading-overlay">
+                    <Loader />
+                </div>
+            )}
             <h2>Zajęcia ({meetings.length})</h2>
+
             {
                 addingNewMeeting
                     ? <NewMeetingForm onSubmit={(meeting) => handleNewMeeting(meeting)}/>
-                    : <button onClick={() => setAddingNewMeeting(true)}>Dodaj nowe spotkanie</button>
+                    : <button
+                          disabled={loading}
+                          onClick={() => setAddingNewMeeting(true)}>
+                        Dodaj nowe spotkanie
+                      </button>
             }
             {meetings.length > 0 &&
-                <MeetingsList meetings={meetings} username={username}
-                              onDelete={handleDeleteMeeting}
-                              onJoin={handleJoinMeeting}
-                              onLeave={handleLeaveMeeting}/>}
+                <MeetingsList
+                    meetings={meetings}
+                    username={username}
+                    onDelete={handleDeleteMeeting}
+                    onJoin={handleJoinMeeting}
+                    onLeave={handleLeaveMeeting}
+                />
+            }
         </div>
     )
+
+
 }
